dbfa6cec2ba02157dea368303c8571b22d99ea9b
SimoneAiello97 <aiellosimone997@gmail.com>
2023-04-13 19:22:07 +0200

    push intermedio

--- m1/s3/g3/javascript/index.js
+++ m1/s3/g3/javascript/index.js
@@ -99,15 +99,17 @@ const changeTitle = function (str) {
     /* ESERCIZIO 10
         Crea una funzione che, al click sul footer, riveli l'URL del link interno come contenuto di un alert()
         */
-       let footer = document.querySelector('footer')
-       let footerLink = footer.querySelector('footer a');
+       let footer = document.getElementsByTagName('footer') 
+           console.log(footer);
+
+       let footerLink = footer[0].querySelector('footer a');
        
        const revealFooterLink = function () {
         alert(footerLink.href)
         
     }
     
-    footer.addEventListener('click', function(){
+    footer[0].addEventListener('click', function(){
         return revealFooterLink();
     });
 
@@ -115,20 +117,50 @@ const changeTitle = function (str) {
         Crea una funzione che crei una tabella nell'elemento con id "tableArea". 
         La tabella avrà 5 elementi e questa struttura: immagine, nome prodotto, quantità, prezzo
     */
+        let target = document.querySelector('#tableArea');
 
-    const generateTable = function () {
-
-
-    }
-
+        let tabella = document.createElement('table');
+        let thead = document.createElement('thead');
+        let tbody = document.createElement('tbody');
+        let primaTr = document.createElement('tr');
+  
+        thead.append(primaTr);
+        tabella.append(thead, tbody);
+        target.append(tabella);
+  
+        let colonne = ['immagine', ' nome prodotto', 'quantità', 'prezzo'];
+  
+        for (let colonna of colonne) {
+            let th = document.createElement('th');
+          th.style.border = '1px solid black';
+          th.textContent = colonna;
+          primaTr.append(th);
+        }
     /* ESERCIZIO 12
         Crea una funzione che aggiunga una riga alla tabella precedentemente creata e fornisca i dati necessari come parametri
     */
+   let nuovaRiga = document.createElement('td');
+   nuovaRiga.style.border = '1px solid black';
+   nuovaRiga.textContent = 'src=cejn cr';
+let nuovaRiga2 = document.createElement('td');
+nuovaRiga2.style.border = '1px solid black';
+nuovaRiga2.textContent = 'oggetto randomico';
 
-    const addRow = function () {
+let nuovaRiga3 = document.createElement('td');
+nuovaRiga3.style.border = '1px solid black';
+nuovaRiga3.textContent = '1000';
+
+let nuovaRiga4 = document.createElement('td');
+nuovaRiga4.style.border = '1px solid black';
+nuovaRiga4.textContent = '15615$';
+    
+        tbody.append(nuovaRiga);
+        tbody.append(nuovaRiga2);
+        tbody.append(nuovaRiga3);
+        tbody.append(nuovaRiga4);
 
 
-    }
+    
 
     /* ESERCIZIO 14
         Crea una funzione che nasconda le immagini della tabella quando eseguita
@@ -143,10 +175,19 @@ const changeTitle = function (str) {
         Crea una funzione che cambi il colore del h2 con id "changeMyColor" con un colore random ad ogni click ricevuto
     */
 
-    const changeColorWithRandom = function () {
-
-
-    }
+        const changeColorWithRandom = function () {
+            let r = Math.floor(Math.random() * 256);
+            let g = Math.floor(Math.random() * 256);
+            let b = Math.floor(Math.random() * 256);
+    
+            let colore = rgb(`${r},${g},${b}`);
+    
+            let titolo = document.getElementById("changeMyColor");
+            titolo.style.color = colore;
+          };
+    
+          let titolo = document.getElementById("changeMyColor");
+          titolo.addEventListener("click", changeColorWithRandom);
 
     /* EXTRA ESERCIZIO 16
         Crea una funzione che elimini le vocali da ogni elemento testuale della pagina (puoi aiutarti con i nuovi metodi degli array di ES6)
