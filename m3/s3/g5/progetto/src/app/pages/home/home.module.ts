8474bbe696f307275fd826cc70e5f0f2d24578c5
SimoneAiello97 <aiellosimone997@gmail.com>
2023-06-11 12:27:51 +0200

    push mattiniero

--- m3/s3/g5/progetto/src/app/pages/home/home.module.ts
+++ m3/s3/g5/progetto/src/app/pages/home/home.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { FormsModule } from '@angular/forms';
+import { PizzaComponent } from './pizza/pizza.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    PizzaComponent
   ],
   imports: [
     CommonModule,
